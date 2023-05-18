e3d722a1108d538151c8920194c66fcec6124b52
Alexson Almeida <as3904527@gmail.com>
2023-05-17 21:46:11 -0300

    errors correction

--- src/main/java/com/seabattle/Seabattle.java
+++ src/main/java/com/seabattle/Seabattle.java
@@ -12,8 +12,11 @@ import javafx.geometry.Orientation;
 
 public class Seabattle {
     private ArrayList<ArrayList<Integer>> sea;
+    private ArrayList<ShipLocation> shipPositions;
+
     public Seabattle() {
         this.sea = new ArrayList<ArrayList<Integer>>();
+
         for (int i = 0; i < 10; i++) {
             ArrayList<Integer> line = new ArrayList<Integer>();
             for (int j = 0; j < 10; j++)
@@ -22,38 +25,63 @@ public class Seabattle {
         }
     }
 
+    /*
+     * @Override
+     * public String toString() {
+     * String seaMap = "   1 2 3 4 5 6 7 8 9 10\n1  ";
+     * int i = 2;
+     * for (ArrayList<Integer> line : sea) {
+     * for (Integer block : line)
+     * seaMap += block ? "1 " : "0 ";
+     * if (i <= 10) {
+     * seaMap += i == 10 ? "\n" + i + " " : "\n" + i + "  ";
+     * i++;
+     * }
+     * }
+     * return seaMap;
+     * }
+     */
 
-/*     @Override
-    public String toString() {
-        String seaMap = "   1 2 3 4 5 6 7 8 9 10\n1  ";
-        int i = 2;
-        for (ArrayList<Integer> line : sea) {
-            for (Integer block : line)
-                seaMap += block ? "1 " : "0 ";
-            if (i <= 10) {
-                seaMap += i == 10 ? "\n" + i + " " : "\n" + i + "  ";
-                i++;
-            }
-        }
-        return seaMap;
-    } */
-
-    public void insertBoat(ShipLocation shipLocation) {
+    public void insertShip(ShipLocation shipLocation) {
         ArrayList<Integer> locations = shipLocation.getLocations();
+        this.shipPositions.add(shipLocation);
+
         for (int i = 0; i + 1 < locations.size(); i += 2)
-            if (this.sea.get(locations.get(i)).get(locations.get(i + 1)) == 1)
+            if (this.sea.get(locations.get(i)).get(locations.get(i + 1)) == 2)
                 throw new ShipAlreadyExistException();
             else
                 this.sea.get(locations.get(i)).set(locations.get(i + 1), 2);
     }
 
-    public void attackPosition(Coordinates coordinate){
+    public boolean turnShip(Coordinates coordinates) {
+        int X = coordinates.getX(), Y = coordinates.getY();
+
+        for (ShipLocation shipPosition : this.shipPositions) {
+            if (shipPosition.getCenter().isTheSamePosition(X, Y)) {
+                this.shipPositions.remove(shipPosition);
+                eraseLocation(shipPosition.getLocations());
+                shipPosition.changeDirections();
+                insertShip(shipPosition);
+                this.shipPositions.add(shipPosition);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void eraseLocation(ArrayList<Integer> locations) {
+        for (int i = 0; i + 1 < locations.size(); i += 2)
+            this.sea.get(locations.get(i)).set(locations.get(i + 1), 0);
+    }
+
+    public void attackPosition(Coordinates coordinate) {
         Integer position = this.sea.get(coordinate.getX()).get(coordinate.getY());
-        
+
         if (position == 0) {
             this.sea.get(coordinate.getX()).add(coordinate.getY(), 1);
         } else {
-            this.sea.get(coordinate.getX()).add(coordinate.getY(), 2);
+            // create here the code to shoot a ship;
         }
     }
 
