aad2ed065c24634c36c98793ea547a90ee444d99
JoseVitorNobre <josevitortinf@gmail.com>
2023-05-19 13:23:48 -0300

    Creating the first window

--- src/main/java/com/seabattle/App.java
+++ src/main/java/com/seabattle/App.java
@@ -1,45 +1,29 @@
 package com.seabattle;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
-import com.seabattle.locationArragment.Coordinates;
-import com.seabattle.locationArragment.ShipLocation;
-import com.seabattle.ships.LargeShip;
-import com.seabattle.ships.MediumShip;
-import com.seabattle.ships.SmallShip;
 
 import javafx.application.Application;
-import javafx.geometry.Orientation;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class App /*  extends Application */ {
+public class App extends Application  {
 
-    // @Override
-    // public void start(Stage stage) {
-    //     String javaVersion = System.getProperty("java.version");
-    //     String javafxVersion = System.getProperty("javafx.version");
-    //     Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    //     Scene scene = new Scene(new StackPane(l), 640, 480);
-    //     stage.setScene(scene);
-    //     stage.show();
-    // }
+    @Override
+    public void start(Stage stage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/layout.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+        stage.setTitle("SeaBattle");
+        stage.setScene(tela);
+        stage.show();
+    }
 
-    // public static void main(String[] args) {
-    //     launch();
-    // }
-
-    public static void main(String[] args) {
-        Player player = new Player();
-        player.placeShip(new ShipLocation(new SmallShip(), Orientation.HORIZONTAL, new Coordinates(3, 3)));
-        player.flipShip(new Coordinates(3, 3));
-        System.out.println(player.getSeabattle());
-
+    public static void main(String[] args) throws Exception {
+        launch();
     }
 
 }
---
+++ src/main/java/com/seabattle/components/Board.java
@@ -0,0 +1,317 @@
+package com.seabattle.components;
+
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class Board {
+
+    @FXML
+    private ImageView waterBlock1;
+
+    @FXML
+    private ImageView waterBlock10;
+
+    @FXML
+    private ImageView waterBlock100;
+
+    @FXML
+    private ImageView waterBlock11;
+
+    @FXML
+    private ImageView waterBlock12;
+
+    @FXML
+    private ImageView waterBlock13;
+
+    @FXML
+    private ImageView waterBlock14;
+
+    @FXML
+    private ImageView waterBlock15;
+
+    @FXML
+    private ImageView waterBlock16;
+
+    @FXML
+    private ImageView waterBlock17;
+
+    @FXML
+    private ImageView waterBlock18;
+
+    @FXML
+    private ImageView waterBlock19;
+
+    @FXML
+    private ImageView waterBlock2;
+
+    @FXML
+    private ImageView waterBlock20;
+
+    @FXML
+    private ImageView waterBlock21;
+
+    @FXML
+    private ImageView waterBlock22;
+
+    @FXML
+    private ImageView waterBlock23;
+
+    @FXML
+    private ImageView waterBlock24;
+
+    @FXML
+    private ImageView waterBlock25;
+
+    @FXML
+    private ImageView waterBlock26;
+
+    @FXML
+    private ImageView waterBlock27;
+
+    @FXML
+    private ImageView waterBlock28;
+
+    @FXML
+    private ImageView waterBlock29;
+
+    @FXML
+    private ImageView waterBlock3;
+
+    @FXML
+    private ImageView waterBlock30;
+
+    @FXML
+    private ImageView waterBlock31;
+
+    @FXML
+    private ImageView waterBlock32;
+
+    @FXML
+    private ImageView waterBlock33;
+
+    @FXML
+    private ImageView waterBlock34;
+
+    @FXML
+    private ImageView waterBlock35;
+
+    @FXML
+    private ImageView waterBlock36;
+
+    @FXML
+    private ImageView waterBlock37;
+
+    @FXML
+    private ImageView waterBlock38;
+
+    @FXML
+    private ImageView waterBlock39;
+
+    @FXML
+    private ImageView waterBlock4;
+
+    @FXML
+    private ImageView waterBlock40;
+
+    @FXML
+    private ImageView waterBlock41;
+
+    @FXML
+    private ImageView waterBlock42;
+
+    @FXML
+    private ImageView waterBlock43;
+
+    @FXML
+    private ImageView waterBlock44;
+
+    @FXML
+    private ImageView waterBlock45;
+
+    @FXML
+    private ImageView waterBlock46;
+
+    @FXML
+    private ImageView waterBlock47;
+
+    @FXML
+    private ImageView waterBlock48;
+
+    @FXML
+    private ImageView waterBlock49;
+
+    @FXML
+    private ImageView waterBlock5;
+
+    @FXML
+    private ImageView waterBlock50;
+
+    @FXML
+    private ImageView waterBlock51;
+
+    @FXML
+    private ImageView waterBlock52;
+
+    @FXML
+    private ImageView waterBlock53;
+
+    @FXML
+    private ImageView waterBlock54;
+
+    @FXML
+    private ImageView waterBlock55;
+
+    @FXML
+    private ImageView waterBlock56;
+
+    @FXML
+    private ImageView waterBlock57;
+
+    @FXML
+    private ImageView waterBlock58;
+
+    @FXML
+    private ImageView waterBlock59;
+
+    @FXML
+    private ImageView waterBlock6;
+
+    @FXML
+    private ImageView waterBlock60;
+
+    @FXML
+    private ImageView waterBlock61;
+
+    @FXML
+    private ImageView waterBlock62;
+
+    @FXML
+    private ImageView waterBlock63;
+
+    @FXML
+    private ImageView waterBlock64;
+
+    @FXML
+    private ImageView waterBlock65;
+
+    @FXML
+    private ImageView waterBlock66;
+
+    @FXML
+    private ImageView waterBlock67;
+
+    @FXML
+    private ImageView waterBlock68;
+
+    @FXML
+    private ImageView waterBlock69;
+
+    @FXML
+    private ImageView waterBlock7;
+
+    @FXML
+    private ImageView waterBlock70;
+
+    @FXML
+    private ImageView waterBlock71;
+
+    @FXML
+    private ImageView waterBlock72;
+
+    @FXML
+    private ImageView waterBlock73;
+
+    @FXML
+    private ImageView waterBlock74;
+
+    @FXML
+    private ImageView waterBlock75;
+
+    @FXML
+    private ImageView waterBlock76;
+
+    @FXML
+    private ImageView waterBlock77;
+
+    @FXML
+    private ImageView waterBlock78;
+
+    @FXML
+    private ImageView waterBlock79;
+
+    @FXML
+    private ImageView waterBlock8;
+
+    @FXML
+    private ImageView waterBlock80;
+
+    @FXML
+    private ImageView waterBlock81;
+
+    @FXML
+    private ImageView waterBlock82;
+
+    @FXML
+    private ImageView waterBlock83;
+
+    @FXML
+    private ImageView waterBlock84;
+
+    @FXML
+    private ImageView waterBlock85;
+
+    @FXML
+    private ImageView waterBlock86;
+
+    @FXML
+    private ImageView waterBlock87;
+
+    @FXML
+    private ImageView waterBlock88;
+
+    @FXML
+    private ImageView waterBlock89;
+
+    @FXML
+    private ImageView waterBlock9;
+
+    @FXML
+    private ImageView waterBlock90;
+
+    @FXML
+    private ImageView waterBlock91;
+
+    @FXML
+    private ImageView waterBlock92;
+
+    @FXML
+    private ImageView waterBlock93;
+
+    @FXML
+    private ImageView waterBlock94;
+
+    @FXML
+    private ImageView waterBlock95;
+
+    @FXML
+    private ImageView waterBlock96;
+
+    @FXML
+    private ImageView waterBlock97;
+
+    @FXML
+    private ImageView waterBlock98;
+
+    @FXML
+    private ImageView waterBlock99;
+
+    private ArrayList<ImageView> waterBlocks;
+
+    public Board() {
+        this.waterBlocks = new ArrayList<ImageView>();
+    }
+
+    
+}
